c91c487ca3351047825021176350717249afa6a6
KevICO <kevin.icol@hotmail.fr>
2020-05-01 18:48:32 +0200

    feat: catch http request of register form.

--- server/src/httpRequests.php
+++ server/src/httpRequests.php
@@ -1,7 +1,32 @@
 <?php 
+    include "database.php";
+
     //Formulaire d'enregistrement :
     if (isset($_POST["registerInformationModalForm"])){
-        echo "Enregistrement";
+
+        //Récupération des inputs :
+        $datas = [
+           "firstName" => strtolower($_POST["firstName"]),
+           "lastName" => strtolower($_POST["lastName"]),
+           "birthDate" => strtotime($_POST["birthDate"]),
+           "socialNumber" => $_POST["socialNumber"],
+           "address" => strtolower($_POST["address"]),
+           "city" => strtolower($_POST["city"]),
+           "postalCode" => $_POST["postalCode"],
+           "phoneNumber" => $_POST["phoneNumber"],
+           "emailAddress" => strtolower($_POST["emailAddress"]),
+           "password" => $_POST["password"],
+        ];
+
+        //Vérification de l'unicité de l'email:
+        $login = $datas["emailAddress"];
+        $query = "SELECT login FROM USERS WHERE login = \"$login\"";
+
+        //Enregistrement des données dans la DB :
+        $result = send_select_request($query);
+        print_r($result);
+        echo("Enregistrement OK");
+
 
     //Formulaire de récupération du mot de passe : 
     }elseif (isset($_POST["forgotPasswordModalForm"])) {
