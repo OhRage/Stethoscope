77d7dd2c3160124151b040de5168833f8fa4c29f
KevICO <kevin.icol@hotmail.fr>
2020-05-03 12:53:28 +0200

    feat: handle of login process in client side.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -32,28 +32,54 @@ function loginButtonsEvent() {
   }
 }
 
-function onLoginClick() {}
+function onLoginClick() {
+  //Récupération du formulaire :
+  let form = document.querySelector("[name=mainForm");
+  let inputs = form.getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type !== "hidden" && inputs[i].type !== "checkbox") {
+      if (inputs[i].value === "") {
+        //Les champs ne doivent pas être vide :
+        inputs[i].setCustomValidity("Invalid field");
+        let invalidFeedback = inputs[i].nextSibling;
+        invalidFeedback.innerHTML = "Vous devez saisir une valeur.";
+      } else {
+        inputs[i].setCustomValidity("");
+      }
+    }
+  }
+
+  if (form.checkValidity() === false) {
+    //Retour à l'écran d'enregistrement :
+    event.preventDefault();
+    event.stopPropagation();
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].type !== "hidden" && inputs[i].type !== "checkbox") {
+        let invalidFeedback = inputs[i].nextSibling;
+        if (inputs[i].checkValidity() === false) {
+          invalidFeedback.className = "invalid-feedback d-block mb-3";
+        } else {
+          invalidFeedback.className = "invalid-feedback";
+        }
+      }
+    }
+  }
+}
 
 function onRegistrationClick() {
   let modalSection = document.getElementById("modalSection");
 
-
   //On ajoute la fenêtre modal au domElement :
-  var modalWindow = new RegistrationModalWindow(
-    modalSection,
-  );
+  var modalWindow = new RegistrationModalWindow(modalSection);
   modalWindow.componentMount();
 }
 
 function onForgotClick() {
-    let modalSection = document.getElementById("modalSection");
+  let modalSection = document.getElementById("modalSection");
 
-
-    //On ajoute la fenêtre modal au domElement :
-    var modalWindow = new ResetPasswordModalWindow(
-      modalSection,
-    );
-    modalWindow.componentMount();
+  //On ajoute la fenêtre modal au domElement :
+  var modalWindow = new ResetPasswordModalWindow(modalSection);
+  modalWindow.componentMount();
 }
 
 //Gestionnnaire d'évenement du document :
