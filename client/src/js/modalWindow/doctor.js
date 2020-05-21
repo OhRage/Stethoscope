070a5627ce264851f2ad6429a1eb0efc38af9f71
KevICO <kevin.icol@hotmail.fr>
2020-05-21 17:36:33 +0200

    feat: creation of doctorModalWindow.

--- client/src/js/modalWindow/doctor.js
+++ client/src/js/modalWindow/doctor.js
@@ -1,10 +1,11 @@
 class DoctorModalWindow {
-    constructor(domElement, serverDatas) {
+    constructor(domElement) {
         this.domElement = domElement;
-        this.serverDatas = serverDatas;
+        this.doctorsDatas = [];
     }
 
     componentMount() {
+        //Construction du composant :
         //Modal :
         let modal = document.createElement("div");
         modal.className = "modal";
@@ -90,37 +91,135 @@ class DoctorModalWindow {
         let modalBody = document.createElement("div");
         modalBody.className = "modal-body";
 
-        let doctorInformationLabel = {
-            firstName: "Prénom :",
-            lastName: "Nom :",
-            birthDate: "Date de naissance :",
-            conventionArea: "Secteur de convention :",
-            address: "Adresse :",
-            city: "Ville :",
-            postalCode: "Code postal :",
-            phoneNumber: "Numéro de téléphone :",
-        };
+        //Liste déroulante des médecins :
+        let doctorCol = document.createElement("div");
+        doctorCol.className = "col-4";
 
-        //Ajout des informations générales :
-        let profileInformationsComponent = new ProfileInformations(
-            modalBody,
-            this.serverDatas,
-            doctorInformationLabel,
-            undefined
+        let doctorLabel = document.createElement("label");
+        doctorLabel.className = "input-group-text";
+        doctorLabel.setAttribute("for", "doctorChoice");
+        doctorLabel.innerHTML = "Choix du médecin : ";
+        doctorCol.appendChild(doctorLabel);
+
+        let doctorOptionList = document.createElement("select");
+        doctorOptionList.className = "custom-select";
+        doctorOptionList.setAttribute("id", "doctorChoice");
+        doctorOptionList.setAttribute("name", "doctorChoice");
+
+        //Gestionnaire d'évenement de la liste medicalType :
+        doctorOptionList.addEventListener("input", () => {
+            this.setDoctorInformations(doctorOptionList);
+        });
+
+        doctorCol.appendChild(doctorOptionList);
+
+        modalBody.appendChild(doctorCol);
+
+        //Photo du médecin :
+        let doctorPhoto = this.doctorPhotoMount();
+        modalBody.appendChild(doctorPhoto);
+
+        //Informations générale du médecin :
+        let doctorInformations = this.doctorInformationsMount();
+        modalBody.appendChild(doctorInformations);
+
+        //Récupération des informations des médecins du cabinets :
+        let ajax = new AjaxCall("getDoctorsMWAjax");
+        ajax.getDoctorsForDoctorModalWindowAjaxOnload(
+            this.doctorsDatas,
+            doctorOptionList
         );
-        profileInformationsComponent.componentMount();
-
-        //Récupération du formulaire :
-        let form = modalBody.querySelector("[name=mainForm");
-        form.setAttribute("id", "doctorInformationModalForm");
-
-        //Ajout d'un input au formulaire contenant le nom de celui (pour traitement côté server):
-        let formName = document.createElement("input");
-        formName.setAttribute("type", "hidden");
-        formName.setAttribute("name", "doctorInformationModalForm");
-
-        form.appendChild(formName);
+        ajax.sendAjax(
+            "GET",
+            "http://stethoscope/server/src/getDoctorDatas.php"
+        );
 
         return modalBody;
+    }
+
+    doctorPhotoMount() {
+        let displayPhoto = document.createElement("div");
+        displayPhoto.className =
+            "row col-6 my-2 justify-content-center align-items-center";
+        let doctorPhoto = document.createElement("img");
+        doctorPhoto.className = "img-fluid";
+        doctorPhoto.setAttribute("id", "doctorPhoto");
+        doctorPhoto.setAttribute("src", "");
+        doctorPhoto.setAttribute("alt", "");
+        displayPhoto.appendChild(doctorPhoto);
+
+        return displayPhoto;
+    }
+
+    doctorInformationsMount() {
+        let doctorInformations = document.createElement("div");
+        doctorInformations.className =
+            "row col-12 my-2 justify-content-center align-items-center";
+
+        let labels = [
+            "Prénom",
+            "Nom",
+            "Age",
+            "Sexe",
+            "Type de médecine",
+            "Numéro de téléphone",
+        ];
+
+        let inputID = [
+            "firstName",
+            "lastName",
+            "age",
+            "sexe",
+            "medicalType",
+            "phoneNumber",
+        ];
+
+        //Prénom :
+        for (let i = 0; i < labels.length; i++) {
+            let row = document.createElement("div");
+            row.className = "row";
+
+            let label = document.createElement("p");
+            label.className = "label";
+            label.innerHTML = labels[i];
+            row.appendChild(label);
+
+            //Construction du paragraphe pour la valeur :
+            let input = document.createElement("input");
+            input.setAttribute("type", "text");
+            input.setAttribute("id", inputID[i]);
+            input.disabled = true;
+            row.appendChild(input);
+
+            doctorInformations.appendChild(row);
+        }
+        return doctorInformations;
+    }
+
+    setDoctorInformations(doctorOptionList) {
+        let doctorModalWindow = document.querySelector("#doctorInformationModal")
+
+        //Récupère la valeur contenu dans la liste déroulante :
+        let doctorID = doctorOptionList.options[doctorOptionList.selectedIndex].value;
+        let doctorDatas = {}
+
+        for( let i = 0; i < this.doctorsDatas.length; i++){
+            if(this.doctorsDatas[i]["doctorID"] === doctorID){
+                doctorDatas = this.doctorsDatas[i]
+                break;
+            }
+        }
+
+        //On affecte la photo du médecin :
+        doctorModalWindow.querySelector("#doctorPhoto").setAttribute("src", doctorDatas["imagePath"])
+        
+        //On change la valeur de tous les inputs :
+        let inputs = doctorModalWindow.querySelectorAll("input");
+        console.log(doctorDatas)
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].setAttribute("value", doctorDatas[inputs[i].getAttribute("id")])
+        }
+
+        
     }
 }
