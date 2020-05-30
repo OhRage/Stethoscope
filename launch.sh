0b4475b42ea1675eeec5ede0795261068857c0b0
KevICO <kevin.icol@hotmail.fr>
2020-05-30 07:53:59 +0200

    stye: addition of a sentence after script execution.

--- launch.sh
+++ launch.sh
@@ -4,3 +4,4 @@ echo "Waiting for mysql install (has about 3 minutes)..."
 sleep 240
 echo "execute sql script create_database.sql"
 docker exec -it $dockerid sh -c "mysql -uroot < app/server/database/script/create_database.sql" 
+echo "Application is OK. Now you can connect localy to Stethoscope at http://localhost:8080"
